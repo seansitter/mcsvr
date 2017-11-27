b402d8f3d97fd04b7fd70044fae40366ebe6dc12
Sean Sitter <sean@seansitter.net>
2017-11-26 20:44:30 -0800

    fatJar + functional test

--- test/client.py
+++ test/client.py
@@ -3,78 +3,87 @@
 from pymemcache.client.base import Client
 import unittest
 import time
+import subprocess
 
 class TestGetMany(unittest.TestCase):
-
     def setUp(self):
+        self.startServer()
         self.client = Client(('localhost', 11211))
 
-    def testGetSingle(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        res = self.client.get('first_key')
-        self.assertEqual(res, "first_value")
+    def startServer(self):
+        proc = subprocess.Popen("java -jar /Users/ssitter/Projects/mcsvr/bin/mcsvr.jar", stdout=subprocess.PIPE)
+        print "PID:", proc.pid
+        print "Return code:", proc.wait()
 
-    def testGetMany(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        self.client.set('second_key', 'second_value', 0, True)
-        res = self.client.get_many(['first_key', 'second_key'])
-        self.assertEqual(res["first_key"], "first_value")
-        self.assertEqual(res["second_key"], "second_value")
+    def testFoo(self):
+        print "HI\n"
 
-    def testGetExpired(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        self.client.set('first_key', 'first_value', 2, False)
-        self.client.get_many(['first_key', 'second_key'])
-        time.sleep(3)
-        self.assertEqual(None, self.client.get('first_key'))
-
-    def testGetDeleted(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        res = self.client.get('first_key')
-        self.assertEqual('first_value', res)
-        self.client.delete('first_key')
-        res = self.client.get('first_key')
-        self.assertEqual(None, res)
-
-    def testUpdateExpired(self):
-        self.client.set('first_key', 'first_value', 2, False)
-        time.sleep(3)
-        self.client.set('first_key', 'second_value', 0, False)
-        res = self.client.get('first_key')
-        self.assertEqual('second_value', res)
-
-    def testCaseUpdateOk(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        res = self.client.gets('first_key')
-        res = self.client.cas('first_key', 'second_value', res[1], 0, False)
-        self.assertTrue(res)
-        res = self.client.get('first_key')
-        self.assertEqual('second_value', res)
-
-    def testCaseUpdateInvalidUniq(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        res = self.client.gets('first_key')
-        casUniq = res[1]
-        self.client.set('first_key', 'second_value', 0, False)
-        res = self.client.cas('first_key', 'third_value', casUniq, 0, False)
-        self.assertFalse(res)
-        res = self.client.get('first_key')
-        self.assertEqual('second_value', res)
-
-    def testCaseUpdateNotOkOnExpired(self):
-        self.client.set('first_key', 'first_value', 2, False)
-        res = self.client.gets('first_key')
-        time.sleep(3)
-        res = self.client.cas('first_key', 'second_value', res[1], 0, False)
-        self.assertEqual(None, res)
-
-    def testCaseUpdateNotOkOnMissing(self):
-        self.client.set('first_key', 'first_value', 0, False)
-        res = self.client.gets('first_key')
-        casUniq = res[1]
-        self.client.delete('first_key')
-        res = self.client.cas('first_key', 'second_value', casUniq, 0, False)
-        self.assertEqual(None, res)
+    #def testGetSingle(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    res = self.client.get('first_key')
+    #    self.assertEqual(res, "first_value")
+#
+    #def testGetMany(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    self.client.set('second_key', 'second_value', 0, True)
+    #    res = self.client.get_many(['first_key', 'second_key'])
+    #    self.assertEqual(res["first_key"], "first_value")
+    #    self.assertEqual(res["second_key"], "second_value")
+#
+    #def testGetExpired(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    self.client.set('first_key', 'first_value', 2, False)
+    #    self.client.get_many(['first_key', 'second_key'])
+    #    time.sleep(3)
+    #    self.assertEqual(None, self.client.get('first_key'))
+#
+    #def testGetDeleted(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    res = self.client.get('first_key')
+    #    self.assertEqual('first_value', res)
+    #    self.client.delete('first_key')
+    #    res = self.client.get('first_key')
+    #    self.assertEqual(None, res)
+#
+    #def testUpdateExpired(self):
+    #    self.client.set('first_key', 'first_value', 2, False)
+    #    time.sleep(3)
+    #    self.client.set('first_key', 'second_value', 0, False)
+    #    res = self.client.get('first_key')
+    #    self.assertEqual('second_value', res)
+#
+    #def testCaseUpdateOk(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    res = self.client.gets('first_key')
+    #    res = self.client.cas('first_key', 'second_value', res[1], 0, False)
+    #    self.assertTrue(res)
+    #    res = self.client.get('first_key')
+    #    self.assertEqual('second_value', res)
+#
+    #def testCaseUpdateInvalidUniq(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    res = self.client.gets('first_key')
+    #    casUniq = res[1]
+    #    self.client.set('first_key', 'second_value', 0, False)
+    #    res = self.client.cas('first_key', 'third_value', casUniq, 0, False)
+    #    self.assertFalse(res)
+    #    res = self.client.get('first_key')
+    #    self.assertEqual('second_value', res)
+#
+    #def testCaseUpdateNotOkOnExpired(self):
+    #    self.client.set('first_key', 'first_value', 2, False)
+    #    res = self.client.gets('first_key')
+    #    time.sleep(3)
+    #    res = self.client.cas('first_key', 'second_value', res[1], 0, False)
+    #    self.assertEqual(None, res)
+#
+    #def testCaseUpdateNotOkOnMissing(self):
+    #    self.client.set('first_key', 'first_value', 0, False)
+    #    res = self.client.gets('first_key')
+    #    casUniq = res[1]
+    #    self.client.delete('first_key')
+    #    res = self.client.cas('first_key', 'second_value', casUniq, 0, False)
+    #    self.assertEqual(None, res)
 
 
 if __name__ == '__main__':
